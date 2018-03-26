29d06085a3e2a7a4c74a9defa3f6f7dfd6ec4746
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-03-26 16:40:57 +0200

    feat: new export page

---
+++ siberian/app/sae/modules/Application/controllers/Advanced/ExportController.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Class Application_Advanced_ExportController
+ */
+class Application_Advanced_ExportController extends Application_Controller_Default
+{
+    /**
+     *
+     */
+    public function indexAction() {
+        $this->loadPartials();
+    }
+
+}
